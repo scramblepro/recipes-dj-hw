8506a864fe6c51c00b9a2da481110f1d8541cc73
Dr.Dre <zaderjkizpnet@gmail.com>
2024-09-18 23:59:26 +0300

    done

---
+++ calculator/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+def recipe_view(request, dish):
+    recipe = DATA.get(dish)  # Получаем рецепт по названию блюда
+    if recipe is None:
+        return HttpResponse(f'<h1>Рецепт для {dish} не найден</h1>', status=404)
+
+    servings = int(request.GET.get('servings', 1))
+
+    scaled_recipe = {ingredient: amount * servings for ingredient, amount in recipe.items()}
+
+    context = {
+        'recipe': scaled_recipe
+    }
+
+    response = '<h1>Ингредиенты для блюда:</h1><ul>'
+    for ingredient, amount in scaled_recipe.items():
+        response += f'<li>{ingredient}: {amount}</li>'
+    response += '</ul>'
+
+    return HttpResponse(response)
